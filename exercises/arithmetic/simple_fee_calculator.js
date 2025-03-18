680162415569b2b8cab7f0ce755753a39e39b9b3
mathmpr <matheusprador@gmail.com>
2025-03-18 19:40:42 -0300

    separate exercises inside a context folder like: arithmetic

---
+++ exercises/arithmetic/simple_fee_calculator.js
@@ -0,0 +1,3 @@
+const readline = require('readline-sync');
+
+// Cálculo de Juros Simples – Peça capital, taxa de juros e tempo para calcular o montante final.
---
+++ exercises/arithmetic/three_number_avarage.js
@@ -0,0 +1,3 @@
+const readline = require('readline-sync');
+
+// Média de Três Números – Solicite três números e calcule a média aritmética.
