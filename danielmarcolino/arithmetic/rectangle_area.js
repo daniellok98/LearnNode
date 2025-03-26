5f9a008fb678e8a615fe7d174d2c57159dc4ece9
Daniel Marcolino <danielmarcolinotrindade@gmail.com>
2025-03-26 18:28:54 -0300

    Resolução do exercício Rectangle Area da categoria arithmetic

--- danielmarcolino/arithmetic/rectangle_area.js
+++ danielmarcolino/arithmetic/rectangle_area.js
@@ -1,3 +1,12 @@
 const readline = require('readline-sync');
 
 // Cálculo de Área de um Retângulo – Solicite a base e a altura de um retângulo e calcule sua área.
+const base = parseFloat(readline.question('Digite a base do retângulo: '));
+const altura = parseFloat(readline.question('Digite a altura do retângulo: '));
+
+if ((base<0) || (altura<0)) {
+    console.log('Por favor, insira valores numéricos válidos. Não existem dimensões negativas em um retângulo.');
+} else {
+    const area = base * altura;
+    console.log(`A área do retângulo é: ${area}`);
+}
